38930ad877fafdc85390fdc9becac393efb74a16
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-08-11 23:55:08 +0300

    Revert "fin"
    This reverts commit a64d24e843af4f99d3543f9a710bace353c88053.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 interface IProduct {
   ref: string;
 }
@@ -18,16 +18,4 @@ export class PortfolioComponent {
     { ref: './assets/images/gallery2.png' },
     { ref: './assets/images/gallery3.png' },
   ];
-
-  @ViewChild('overlay', { static: false }) overlay!: ElementRef;
-  show(event: HTMLImageElement): void {
-    this.overlay.nativeElement.classList.remove('d-none');
-    this.overlay.nativeElement.innerHTML = `
-    <img src="${event.src}" class="rounded-3 w-50" alt="">`;
-  }
-  hide(event: MouseEvent): void {
-    if (event.target === event.currentTarget) {
-      this.overlay.nativeElement.classList.add('d-none');
-    }
-  }
 }
