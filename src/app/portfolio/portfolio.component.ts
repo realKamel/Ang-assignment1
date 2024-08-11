5644f270af53eb6a5b2b5345c47b233244eec956
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-08-12 00:08:39 +0300

    fin

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 interface IProduct {
   ref: string;
 }
@@ -18,4 +18,17 @@ export class PortfolioComponent {
     { ref: './assets/images/gallery2.png' },
     { ref: './assets/images/gallery3.png' },
   ];
+  @ViewChild('overlay', { static: false }) overlay!: ElementRef;
+
+  show(e: HTMLImageElement) {
+    this.overlay.nativeElement.classList.remove('d-none');
+    this.overlay.nativeElement.innerHTML = `
+    <img src="${e.src}" class="rounded-3 w-50" alt="">
+    `;
+  }
+  hide(e: MouseEvent) {
+    if (e.target === e.currentTarget) {
+      this.overlay.nativeElement.classList.add('d-none');
+    }
+  }
 }
